65cb295e05a21e753b2edf68e3d21c7268008658
Shaun Walker <shaun.walker@siliqon.com>
2019-09-04 12:42:22 -0400

    Refactored Settings to use Dictionary

--- Oqtane.Client/Services/Interfaces/ISettingService.cs
+++ Oqtane.Client/Services/Interfaces/ISettingService.cs
@@ -6,32 +6,34 @@ namespace Oqtane.Services
 {
     public interface ISettingService
     {
-        Task<List<Setting>> GetHostSettingsAsync();
+        Task<Dictionary<string, string>> GetHostSettingsAsync();
 
-        Task<Setting> UpdateHostSettingsAsync(List<Setting> HostSettings, string SettingName, string SettingValue);
+        Task UpdateHostSettingsAsync(Dictionary<string, string> HostSettings);
 
-        Task<List<Setting>> GetSiteSettingsAsync(int SiteId);
+        Task<Dictionary<string, string>> GetSiteSettingsAsync(int SiteId);
 
-        Task<Setting> UpdateSiteSettingsAsync(List<Setting> SiteSettings, int SiteId, string SettingName, string SettingValue);
+        Task UpdateSiteSettingsAsync(Dictionary<string, string> SiteSettings, int SiteId);
 
-        Task<List<Setting>> GetPageSettingsAsync(int PageId);
+        Task<Dictionary<string, string>> GetPageSettingsAsync(int PageId);
 
-        Task<Setting> UpdatePageSettingsAsync(List<Setting> PageSettings, int PageId, string SettingName, string SettingValue);
+        Task UpdatePageSettingsAsync(Dictionary<string, string> PageSettings, int PageId);
 
-        Task<List<Setting>> GetPageModuleSettingsAsync(int PageModuleId);
+        Task<Dictionary<string, string>> GetPageModuleSettingsAsync(int PageModuleId);
 
-        Task<Setting> UpdatePageModuleSettingsAsync(List<Setting> PageModuleSettings, int PageModuleId, string SettingName, string SettingValue);
+        Task UpdatePageModuleSettingsAsync(Dictionary<string, string> PageModuleSettings, int PageModuleId);
 
-        Task<List<Setting>> GetModuleSettingsAsync(int ModuleId);
+        Task<Dictionary<string, string>> GetModuleSettingsAsync(int ModuleId);
 
-        Task<Setting> UpdateModuleSettingsAsync(List<Setting> ModuleSettings, int ModuleId, string SettingName, string SettingValue);
+        Task UpdateModuleSettingsAsync(Dictionary<string, string> ModuleSettings, int ModuleId);
 
-        Task<List<Setting>> GetUserSettingsAsync(int UserId);
+        Task<Dictionary<string, string>> GetUserSettingsAsync(int UserId);
 
-        Task<Setting> UpdateUserSettingsAsync(List<Setting> UserSettings, int UserId, string SettingName, string SettingValue);
+        Task UpdateUserSettingsAsync(Dictionary<string, string> UserSettings, int UserId);
 
+        Task<Dictionary<string, string>> GetSettingsAsync(string EntityName, int EntityId);
 
-        Task<List<Setting>> GetSettingsAsync(string EntityName, int EntityId);
+        Task UpdateSettingsAsync(Dictionary<string, string> Settings, string EntityName, int EntityId);
+
 
         Task<Setting> GetSettingAsync(int SettingId);
 
@@ -39,13 +41,11 @@ namespace Oqtane.Services
 
         Task<Setting> UpdateSettingAsync(Setting Setting);
 
-        Task<Setting> UpdateSettingsAsync(List<Setting> Settings, string EntityName, int EntityId, string SettingName, string SettingValue);
-
         Task DeleteSettingAsync(int SettingId);
 
 
-        string GetSetting(List<Setting> Settings, string SettingName, string DefaultValue);
+        string GetSetting(Dictionary<string, string> Settings, string SettingName, string DefaultValue);
 
-        List<Setting> SetSetting(List<Setting> Settings, string EntityName, int EntityId, string SettingName, string SettingValue);
+        Dictionary<string, string> SetSetting(Dictionary<string, string> Settings, string SettingName, string SettingValue);
      }
 }
