d13f62d891ca33441d14c2ec90e5e784c8b0c565
Emanuele Filardo <dev@n4notech.it>
2019-09-19 16:52:47 +0200

    Add basic support for SoftDeletable

---
+++ Oqtane.Shared/Models/ISoftDeletable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Oqtane.Models
+{
+    public interface ISoftDeletable
+    {
+        string DeletedBy { get; set; }
+        DateTime? DeletedOn { get; set; }
+        bool IsSoftDeleted { get; set; }
+    }
+}
