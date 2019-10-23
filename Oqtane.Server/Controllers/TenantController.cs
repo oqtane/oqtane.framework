db3309d9dd1d4f6678c1b1f6ce9f89b3aa2e8f5e
Shaun Walker <shaun.walker@siliqon.com>
2019-10-23 15:09:06 -0400

    rollback security change as it affects installer - will need to evaluate more thoroughly

--- Oqtane.Server/Controllers/TenantController.cs
+++ Oqtane.Server/Controllers/TenantController.cs
@@ -22,7 +22,6 @@ namespace Oqtane.Controllers
 
         // GET: api/<controller>
         [HttpGet]
-        [Authorize(Roles = Constants.HostRole)]
         public IEnumerable<Tenant> Get()
         {
             return Tenants.GetTenants();
@@ -30,7 +29,6 @@ namespace Oqtane.Controllers
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
-        [Authorize(Roles = Constants.HostRole)]
         public Tenant Get(int id)
         {
             return Tenants.GetTenant(id);
