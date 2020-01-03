cbe33c560f29c23b36532ae1a5ea82d18b38e634
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-01-03 20:04:56 +0300

    Move MvcBuilderExtensions to Extensions folder

--- Oqtane.Server/Extensions/OqtaneMvcBuilderExtensions.cs
+++ Oqtane.Server/Extensions/OqtaneMvcBuilderExtensions.cs
@@ -1,19 +1,18 @@
 ﻿using System.Linq;
-using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class MvcModuleExtensions
+    public static class OqtaneMvcBuilderExtensions
     {
         public static IMvcBuilder AddOqtaneApplicationParts(this IMvcBuilder mvcBuilder)
         {
             // load MVC application parts from module assemblies
-            foreach (Assembly assembly in OqtaneServiceCollectionExtensions.GetOqtaneModuleAssemblies())
+            foreach (var assembly in OqtaneServiceCollectionExtensions.GetOqtaneModuleAssemblies())
             {
                 // check if assembly contains MVC Controllers
-                if (assembly.GetTypes().Where(item => item.IsSubclassOf(typeof(Controller))).ToArray().Length > 0)
+                if (assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(Controller))).ToArray().Length > 0)
                 {
                     var partFactory = ApplicationPartFactory.GetApplicationPartFactory(assembly);
                     foreach (var part in partFactory.GetApplicationParts(assembly))
