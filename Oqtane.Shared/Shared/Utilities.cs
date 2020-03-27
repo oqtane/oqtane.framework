f8d77320250540d99484f519aaace656066ed162
Aubrey <aubrey.b@treskcow.tech>
2020-03-27 13:42:14 -0400

    Moved logic to  the Utilities class.

--- Oqtane.Shared/Shared/Utilities.cs
+++ Oqtane.Shared/Shared/Utilities.cs
@@ -1,5 +1,7 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Oqtane.Shared
 {
@@ -216,6 +218,19 @@ namespace Oqtane.Shared
             {
                 return "";
             }
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (email != "")
+            {
+                return true;
+            }
+
+            return Regex.IsMatch(email,
+                  @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                  @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                  RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
         }
     }
 }
