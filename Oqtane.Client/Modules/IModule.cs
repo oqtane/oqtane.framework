a85e5d02d269a5107ac1c49b6c6bedbe297c1f95
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-04-02 19:42:02 +0300

    Mark Properties as obsolete

--- Oqtane.Client/Modules/IModule.cs
+++ Oqtane.Client/Modules/IModule.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Oqtane.Models;
 
 namespace Oqtane.Modules
 {
     public interface IModule
     {
+        [Obsolete("This properties is deprecated, please use ModuleDeinition to define module metadata instead.")]
         Dictionary<string, string> Properties { get; }
 
         ModuleDefinition ModuleDefinition { get; }
