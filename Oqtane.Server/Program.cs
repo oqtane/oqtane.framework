38126c48d9ffaca8ac636c134622e696c531779c
Shaun Walker <shaun.walker@siliqon.com>
2020-04-02 12:06:07 -0400

    upgrade to .NET Core 3.2 Preview 3 and fixes for issues created by #314

--- Oqtane.Server/Program.cs
+++ Oqtane.Server/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 // DO NOT REMOVE - needed for client-side Blazor
-using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,31 +11,15 @@ namespace Oqtane.Server
 {
     public class Program
     {
-#if DEBUG || RELEASE
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
+            var host = BuildWebHost(args);
             using (var serviceScope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
                 var manager = serviceScope.ServiceProvider.GetService<DatabaseManager>();
                 manager.StartupMigration();
             }
-            //DatabaseManager.StartupMigration();                    
             host.Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-#endif
-
-#if WASM
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
@@ -45,7 +29,5 @@ namespace Oqtane.Server
                     .Build())
                 .UseStartup<Startup>()
                 .Build();
-#endif
-
     }
 }
