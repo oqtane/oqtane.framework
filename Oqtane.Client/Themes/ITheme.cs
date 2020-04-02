bbdb1fedcc4de66464de0c65af289292f000ec29
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-04-02 21:24:10 +0300

    Mark Properties as Obsolete

--- Oqtane.Client/Themes/ITheme.cs
+++ Oqtane.Client/Themes/ITheme.cs
@@ -6,6 +6,7 @@ namespace Oqtane.Themes
 {
     public interface ITheme
     {
+        [Obsolete("This property is deprecated, please use ModuleDeinition to define theme metadata instead.")]
         Dictionary<string, string> Properties { get; }
 
         ModuleDefinition ModuleDefinition { get; }
