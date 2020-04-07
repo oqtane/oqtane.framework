4e017172d45cedfe4029b95d68fc93425932cf4a
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-04-07 21:48:23 +0300

    Fix NavigationUrl unit tests

--- Oqtane.Test/Oqtane.Shared.Tests/UtilitiesTests.cs
+++ Oqtane.Test/Oqtane.Shared.Tests/UtilitiesTests.cs
@@ -8,13 +8,13 @@ namespace Oqtane.Test.Oqtane.Shared.Tests
         [Theory]
         [InlineData("contoso", "login", "returnUrl=/admin", "/contoso/login?returnUrl=/admin")]
         [InlineData("contoso", "admin", "", "/contoso/admin")]
-        [InlineData("contoso", "", "pageId=4", "/contoso?pageId=4")]
-        [InlineData("contoso", "", "pageId=4&moduleId=10", "/contoso?pageId=4&moduleId=10")]
+        [InlineData("contoso", "", "pageId=4", "/contoso/?pageId=4")]
+        [InlineData("contoso", "", "pageId=4&moduleId=10", "/contoso/?pageId=4&moduleId=10")]
         [InlineData("contoso", "", "", "/contoso/")]
         [InlineData("", "login", "returnUrl=/admin", "/login?returnUrl=/admin")]
         [InlineData("", "admin", "", "/admin")]
-        [InlineData("", "", "pageId=4", "?pageId=4")]
-        [InlineData("", "", "pageId=4&moduleId=10", "?pageId=4&moduleId=10")]
+        [InlineData("", "", "pageId=4", "/?pageId=4")]
+        [InlineData("", "", "pageId=4&moduleId=10", "/?pageId=4&moduleId=10")]
         [InlineData("", "", "", "/")]
         public void NavigateUrlTest(string alias, string path, string parameters, string expectedUrl)
         {
