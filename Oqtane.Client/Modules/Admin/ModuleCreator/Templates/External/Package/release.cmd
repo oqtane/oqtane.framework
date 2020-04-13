f41dd06f6dc31abe4f6fd5b1e614ef99c925607f
Shaun Walker <shaun.walker@siliqon.com>
2020-04-13 12:48:49 -0400

    fixed external module template so that it generates folder paths correctly

--- Oqtane.Client/Modules/Admin/ModuleCreator/Templates/External/Package/release.cmd
+++ Oqtane.Client/Modules/Admin/ModuleCreator/Templates/External/Package/release.cmd
@@ -1,2 +1,2 @@
-"..\..\oqtane.framework\oqtane.package\nuget.exe" pack [Owner].[Module]s.Module.nuspec 
-XCOPY "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\wwwroot\Modules\" /Y
+"..\..\[RootFolder]\oqtane.package\nuget.exe" pack [Owner].[Module]s.Module.nuspec 
+XCOPY "*.nupkg" "..\..\[RootFolder]\Oqtane.Server\wwwroot\Modules\" /Y
