7d896709305249a688831cbdef75ef65b5a1e00c
Mike Casas <mike.casas.public@gmail.com>
2020-06-18 12:58:30 -0400

    Fixed the sql script file name and added update for null values.

--- Oqtane.Server/Scripts/Tenant.01.00.01.01.sql
+++ Oqtane.Server/Scripts/Tenant.01.00.01.01.sql
@@ -7,3 +7,6 @@ Version 1.0.1 Notification migration script
 ALTER TABLE [dbo].[Notification] ADD
 	[SendOn] [datetime] NULL
 GO
+
+UPDATE [dbo].[Notification] SET SendOn = CreatedOn WHERE SendOn IS NULL
+GO
