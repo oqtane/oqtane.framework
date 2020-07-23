bcc33af52bfef09590ae0f3775205343ee5db3f7
Shaun Walker <shaun.walker@siliqon.com>
2020-07-23 15:31:28 -0400

    create 1.0.2 packages

--- Oqtane.Package/release.cmd
+++ Oqtane.Package/release.cmd
@@ -1,5 +1,7 @@
 DEL "*.nupkg"
 dotnet clean -c Release ..\Oqtane.sln 
 dotnet build -c Release ..\Oqtane.sln 
-dotnet pack  -o .\ -c Release ..\Oqtane.sln 
-nuget.exe pack Oqtane.Framework.nuspec                    
+nuget.exe pack Oqtane.Framework.nuspec
+nuget.exe pack Oqtane.Client.nuspec
+nuget.exe pack Oqtane.Server.nuspec
+nuget.exe pack Oqtane.Shared.nuspec                  
