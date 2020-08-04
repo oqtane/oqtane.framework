94f0bdcce96c136822595adbc6fa9a05f2285253
Shaun Walker <shaun.walker@siliqon.com>
2020-08-04 08:47:39 -0400

    Improvements to System Update

--- Oqtane.Package/release.cmd
+++ Oqtane.Package/release.cmd
@@ -1,6 +1,7 @@
-DEL "*.nupkg"
+del "*.nupkg"
 dotnet clean -c Release ..\Oqtane.sln 
 dotnet build -c Release ..\Oqtane.sln 
+dotnet publish ..\Oqtane.Server\Oqtane.Server.csproj /p:Configuration=Release
 nuget.exe pack Oqtane.Framework.nuspec
 nuget.exe pack Oqtane.Client.nuspec
 nuget.exe pack Oqtane.Server.nuspec
