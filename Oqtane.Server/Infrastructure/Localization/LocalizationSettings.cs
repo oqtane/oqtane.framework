0796ce54a9a04c9cb9a1a73b26c7149dc565b127
hishamco <hishamco_2007@yahoo.com>
2020-09-29 17:30:56 +0300

    Add localization settings

---
+++ Oqtane.Server/Infrastructure/Localization/LocalizationSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Oqtane.Infrastructure.Localization
+{
+    public static class LocalizationSettings
+    {
+        private const string EnglishCulture = "en-US";
+
+        static LocalizationSettings()
+        {
+            DefaultCulture = EnglishCulture;
+            SupportedCultures = new List<string> { DefaultCulture };
+        }
+
+        public static string DefaultCulture { get; set; }
+
+        public static IList<string> SupportedCultures { get; set; }
+    }
+}
