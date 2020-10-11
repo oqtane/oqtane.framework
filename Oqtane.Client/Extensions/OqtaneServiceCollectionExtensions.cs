805d3431d6b3d0857104da216ec53e94881ed76b
hishamco <hishamco_2007@yahoo.com>
2020-10-11 17:06:57 +0300

    Add oqtane services

--- Oqtane.Client/Extensions/OqtaneServiceCollectionExtensions.cs
+++ Oqtane.Client/Extensions/OqtaneServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
 using Oqtane.Providers;
+using Oqtane.Services;
+using Oqtane.Shared;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -10,6 +12,37 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddAuthorizationCore();
             services.AddScoped<IdentityAuthenticationStateProvider>();
             services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+
+            return services;
+        }
+
+        public static IServiceCollection AddOqtaneServices(this IServiceCollection services)
+        {
+            services.AddScoped<SiteState>();
+            services.AddScoped<IInstallationService, InstallationService>();
+            services.AddScoped<IModuleDefinitionService, ModuleDefinitionService>();
+            services.AddScoped<IThemeService, ThemeService>();
+            services.AddScoped<IAliasService, AliasService>();
+            services.AddScoped<ITenantService, TenantService>();
+            services.AddScoped<ISiteService, SiteService>();
+            services.AddScoped<IPageService, PageService>();
+            services.AddScoped<IModuleService, ModuleService>();
+            services.AddScoped<IPageModuleService, PageModuleService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IProfileService, ProfileService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<IUserRoleService, UserRoleService>();
+            services.AddScoped<ISettingService, SettingService>();
+            services.AddScoped<IPackageService, PackageService>();
+            services.AddScoped<ILogService, LogService>();
+            services.AddScoped<IJobService, JobService>();
+            services.AddScoped<IJobLogService, JobLogService>();
+            services.AddScoped<INotificationService, NotificationService>();
+            services.AddScoped<IFolderService, FolderService>();
+            services.AddScoped<IFileService, FileService>();
+            services.AddScoped<ISiteTemplateService, SiteTemplateService>();
+            services.AddScoped<ISqlService, SqlService>();
+            services.AddScoped<ISystemService, SystemService>();
 
             return services;
         }
--- Oqtane.Client/Program.cs
+++ Oqtane.Client/Program.cs
@@ -29,33 +29,7 @@ namespace Oqtane.Client
             builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
             builder.Services.AddOqtaneAuthentication();
-
-            // register scoped core services
-            builder.Services.AddScoped<SiteState>();
-            builder.Services.AddScoped<IInstallationService, InstallationService>();
-            builder.Services.AddScoped<IModuleDefinitionService, ModuleDefinitionService>();
-            builder.Services.AddScoped<IThemeService, ThemeService>();
-            builder.Services.AddScoped<IAliasService, AliasService>();
-            builder.Services.AddScoped<ITenantService, TenantService>();
-            builder.Services.AddScoped<ISiteService, SiteService>();
-            builder.Services.AddScoped<IPageService, PageService>();
-            builder.Services.AddScoped<IModuleService, ModuleService>();
-            builder.Services.AddScoped<IPageModuleService, PageModuleService>();
-            builder.Services.AddScoped<IUserService, UserService>();
-            builder.Services.AddScoped<IProfileService, ProfileService>();
-            builder.Services.AddScoped<IRoleService, RoleService>();
-            builder.Services.AddScoped<IUserRoleService, UserRoleService>();
-            builder.Services.AddScoped<ISettingService, SettingService>();
-            builder.Services.AddScoped<IPackageService, PackageService>();
-            builder.Services.AddScoped<ILogService, LogService>();
-            builder.Services.AddScoped<IJobService, JobService>();
-            builder.Services.AddScoped<IJobLogService, JobLogService>();
-            builder.Services.AddScoped<INotificationService, NotificationService>();
-            builder.Services.AddScoped<IFolderService, FolderService>();
-            builder.Services.AddScoped<IFileService, FileService>();
-            builder.Services.AddScoped<ISiteTemplateService, SiteTemplateService>();
-            builder.Services.AddScoped<ISqlService, SqlService>();
-            builder.Services.AddScoped<ISystemService, SystemService>();
+            builder.Services.AddOqtaneServices();
 
             await LoadClientAssemblies(httpClient);
 
