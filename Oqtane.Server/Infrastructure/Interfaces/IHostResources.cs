9294537e23156754b9860bb721d01ef30f04562e
Shaun Walker <shaun.walker@siliqon.com>
2020-10-19 08:50:37 -0400

    modify comment

--- Oqtane.Server/Infrastructure/Interfaces/IHostResources.cs
+++ Oqtane.Server/Infrastructure/Interfaces/IHostResources.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 
 namespace Oqtane.Infrastructure
 {
-    // Obsolete - use the Resources collection as part of IModuleControl or IThemeCOntrol and use the ResourceDeclaration.Global property
-    // note - not using the [Obsolete] attribute in this case as we want to avoid compilation warnings in the core framework
+    // this interface is for declaring global resources and is useful for scenarios where you want to declare resources in a single location for the entire application
     public interface IHostResources
     {
         List<Resource> Resources { get; } // identifies global resources for an application
