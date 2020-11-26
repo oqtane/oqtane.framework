c2e7ed206ebde8fde65935eaf35bbf5225899e22
hishamco <hishamco_2007@yahoo.com>
2020-11-26 22:29:27 +0300

    Add Order to Startup classes

--- Oqtane.Server/Infrastructure/Interfaces/IServerStartup.cs
+++ Oqtane.Server/Infrastructure/Interfaces/IServerStartup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,11 +6,19 @@ namespace Oqtane.Infrastructure
 {
     public interface IServerStartup
     {
+        /// <summary>
+        /// Gets the execution order.
+        /// </summary>
+        /// <remarks>The default services should (only) have negative values, so they can be executed first.</remarks>
+        int Order { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         void ConfigureServices(IServiceCollection services);
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         void Configure(IApplicationBuilder app, IWebHostEnvironment env);
+
         void ConfigureMvc(IMvcBuilder mvcBuilder);
     }
 }
--- Oqtane.Shared/Interfaces/IClientStartup.cs
+++ Oqtane.Shared/Interfaces/IClientStartup.cs
@@ -1,10 +1,16 @@
-﻿
+
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Oqtane.Services
 {
     public interface IClientStartup
     {
+        /// <summary>
+        /// Gets the execution order.
+        /// </summary>
+        /// <remarks>The default services should (only) have negative values, so they can be executed first.</remarks>
+        int Order { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         void ConfigureServices(IServiceCollection services);
     }
