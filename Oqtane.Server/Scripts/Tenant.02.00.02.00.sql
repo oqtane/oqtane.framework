21e09d95da17aaaf297b52e68179bf3451b4cd32
hishamco <hishamco_2007@yahoo.com>
2021-01-10 21:32:25 +0300

    Add migration script

---
+++ Oqtane.Server/Scripts/Tenant.02.00.02.00.sql
@@ -0,0 +1,26 @@
+/*  
+
+Version 2.0.0 Tenant migration script
+
+*/
+
+CREATE TABLE [dbo].[Language](
+	[LanguageId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+    [Code] [nvarchar](10) NOT NULL,
+    [TenantId] [int],
+	[CreatedBy] [nvarchar](256) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[ModifiedBy] [nvarchar](256) NOT NULL,
+	[ModifiedOn] [datetime] NOT NULL,
+  CONSTRAINT [PK_Language] PRIMARY KEY CLUSTERED 
+  (
+	[LanguageId] ASC
+  )
+)
+GO
+
+ALTER TABLE [dbo].[Language]  WITH CHECK ADD  CONSTRAINT [FK_Language_Tenant] FOREIGN KEY([TenantId])
+REFERENCES [dbo].[Tenant] ([TenantId])
+ON DELETE CASCADE
+GO
