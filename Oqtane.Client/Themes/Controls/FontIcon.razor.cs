5a02ce612481a1bc33fdc9e48ee35e02990fbe5a
Jayson Furr <jayson.furr@outlook.com>
2021-01-23 21:14:44 -0600

    Fixes to vertical menu logic. Now supports multiple levels of menu items. Added FontIcon component to reduce duplicate code.

---
+++ Oqtane.Client/Themes/Controls/FontIcon.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Oqtane.Themes.Controls
+{
+    public partial class FontIcon : ComponentBase
+    {
+        [Parameter()]
+        public string Value { get; set; }
+    }
+}
