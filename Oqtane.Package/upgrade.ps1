0fe931d772179404287a5b4f5ac3b52b3fceb77c
Shaun Walker <shaun.walker@siliqon.com>
2021-04-19 08:04:33 -0400

    prepare for 2.0.2 release

--- Oqtane.Package/upgrade.ps1
+++ Oqtane.Package/upgrade.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path "..\Oqtane.Server\bin\Release\net5.0\publish\*" -DestinationPath "..\Oqtane.Server\bin\Release\Oqtane.Framework.2.0.1.Upgrade.zip" -Force
+Compress-Archive -Path "..\Oqtane.Server\bin\Release\net5.0\publish\*" -DestinationPath "..\Oqtane.Server\bin\Release\Oqtane.Framework.2.0.2.Upgrade.zip" -Force
