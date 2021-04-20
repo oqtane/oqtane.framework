8f5beaf3fe7beb81d2aae7268c151922299dcaf4
Charles Nurse <charlesnurse@gmail.com>
2021-04-20 10:58:26 -0700

    Fix bug with installs due to missing Migrations

---
+++ Oqtane.Server/Migrations/02000202_UpdateDefaultContainerTypeInSitePage.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Oqtane.Interfaces;
+using Oqtane.Migrations.EntityBuilders;
+using Oqtane.Repository;
+
+namespace Oqtane.Migrations
+{
+    [DbContext(typeof(TenantDBContext))]
+    [Migration("Tenant.02.00.02.02")]
+    public class UpdateDefaultContainerTypeInSitePage : MultiDatabaseMigration
+    {
+        public UpdateDefaultContainerTypeInSitePage(IEnumerable<IOqtaneDatabase> databases) : base(databases)
+        {
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (ActiveDatabase.Name == "SqlServer" || ActiveDatabase.Name == "LocalDB")
+            {
+                //Update DefaultContainerType In Site
+                var siteEntityBuilder = new SiteEntityBuilder(migrationBuilder, ActiveDatabase);
+                siteEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.DefaultTitle, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'");
+                siteEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.DefaultNoTitle, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.NoTitle, Oqtane.Client'");
+
+                //Update DefaultContainerType in Page
+                var pageEntityBuilder = new PageEntityBuilder(migrationBuilder, ActiveDatabase);
+                pageEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.DefaultTitle, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'");
+                pageEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.DefaultNoTitle, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.NoTitle, Oqtane.Client'");
+
+                //Update ContainerType in PageModule
+                var pageModuleEntityBuilder = new PageModuleEntityBuilder(migrationBuilder, ActiveDatabase);
+                pageModuleEntityBuilder.UpdateColumn("ContainerType", "'Oqtane.Themes.OqtaneTheme.DefaultTitle, Oqtane.Client'", "ContainerType = 'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'");
+                pageModuleEntityBuilder.UpdateColumn("ContainerType", "'Oqtane.Themes.OqtaneTheme.DefaultNoTitle, Oqtane.Client'", "ContainerType = 'Oqtane.Themes.OqtaneTheme.NoTitle, Oqtane.Client'");
+            }
+
+        }
+    }
+}
---
+++ Oqtane.Server/Migrations/02000203_DropDefaultLayoutInSite.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Oqtane.Interfaces;
+using Oqtane.Migrations.EntityBuilders;
+using Oqtane.Repository;
+
+namespace Oqtane.Migrations
+{
+    [DbContext(typeof(TenantDBContext))]
+    [Migration("Tenant.02.00.02.03")]
+    public class DropDefaultLayoutInSite : MultiDatabaseMigration
+    {
+        public DropDefaultLayoutInSite(IEnumerable<IOqtaneDatabase> databases) : base(databases)
+        {
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (ActiveDatabase.Name == "SqlServer" || ActiveDatabase.Name == "LocalDB")
+            {
+                //Alter Column in Setting table for Sql Server
+                var settingEntityBuilder = new SettingEntityBuilder(migrationBuilder, ActiveDatabase);
+                settingEntityBuilder.DropIndex("IX_Setting");
+                settingEntityBuilder.AlterStringColumn("SettingName", 200);
+                settingEntityBuilder.AddIndex("IX_Setting", new [] {"EntityName", "EntityId", "SettingName"}, true);
+
+                //Drop Column from Site Table
+                var siteEntityBuilder = new SiteEntityBuilder(migrationBuilder, ActiveDatabase);
+                siteEntityBuilder.DropColumn("DefaultLayoutType");
+
+                //Update DefaultContainerType In Site
+                siteEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.DefaultTitle, Oqtane.Client'");
+                siteEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.DefaultNoTitle, Oqtane.Client'");
+
+                //Drop Column from Page Table
+                var pageEntityBuilder = new PageEntityBuilder(migrationBuilder, ActiveDatabase);
+                pageEntityBuilder.DropColumn("LayoutType");
+
+                //Update DefaultContainerType in Page
+                pageEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.DefaultTitle, Oqtane.Client'");
+                pageEntityBuilder.UpdateColumn("DefaultContainerType", "'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'", "DefaultContainerType = 'Oqtane.Themes.OqtaneTheme.DefaultNoTitle, Oqtane.Client'");
+
+
+                //Update ContainerType in PageModule
+                var pageModuleEntityBuilder = new PageModuleEntityBuilder(migrationBuilder, ActiveDatabase);
+                pageModuleEntityBuilder.UpdateColumn("ContainerType", "'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'", "ContainerType = 'Oqtane.Themes.OqtaneTheme.DefaultTitle, Oqtane.Client'");
+                pageModuleEntityBuilder.UpdateColumn("ContainerType", "'Oqtane.Themes.OqtaneTheme.Container, Oqtane.Client'", "ContainerType = 'Oqtane.Themes.OqtaneTheme.DefaultNoTitle, Oqtane.Client'");
+
+            }
+        }
+    }
+}
