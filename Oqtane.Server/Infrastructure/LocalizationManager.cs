76c2a2f2f9ca994432201b154fc4d7f1ee9d79cc
hishamco <hishamco_2007@yahoo.com>
2021-04-26 01:55:27 +0300

    Add English to supported cultures by default

--- Oqtane.Server/Infrastructure/LocalizationManager.cs
+++ Oqtane.Server/Infrastructure/LocalizationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
@@ -9,7 +10,7 @@ namespace Oqtane.Infrastructure
     public class LocalizationManager : ILocalizationManager
     {
         private static readonly string DefaultCulture = Constants.DefaultCulture;
-        private static readonly string[] SupportedCultures = new[] { DefaultCulture };
+        private static readonly string[] DefaultSupportedCultures = new[] { DefaultCulture };
 
         private readonly LocalizationOptions _localizationOptions;
 
@@ -19,25 +20,19 @@ namespace Oqtane.Infrastructure
         }
 
         public string GetDefaultCulture()
-            => string.IsNullOrEmpty(_localizationOptions.DefaultCulture)
+            => String.IsNullOrEmpty(_localizationOptions.DefaultCulture)
                 ? DefaultCulture
                 : _localizationOptions.DefaultCulture;
 
         public string[] GetSupportedCultures()
         { 
-            List<string> cultures = new List<string>();
+            var cultures = new List<string>(DefaultSupportedCultures);
             foreach(var file in Directory.EnumerateFiles(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Oqtane.Client.resources.dll", SearchOption.AllDirectories))
             {
                 cultures.Add(Path.GetFileName(Path.GetDirectoryName(file)));
             }
-            if (cultures.Count == 0)
-            {
-                return SupportedCultures;
-            }
-            else
-            {
-                return cultures.ToArray();
-            }
+
+            return cultures.ToArray();
         }
     }
 }
