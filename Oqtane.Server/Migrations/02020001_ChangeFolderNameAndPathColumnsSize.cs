795e71278b2a07f0dc30de5eaec8dbee8f4f02d2
hishamco <hishamco_2007@yahoo.com>
2021-05-16 20:28:41 +0300

    Change Folder Name & Path length

---
+++ Oqtane.Server/Migrations/02020001_ChangeFolderNameAndPathColumnsSize.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Oqtane.Interfaces;
+using Oqtane.Migrations.EntityBuilders;
+using Oqtane.Repository;
+
+namespace Oqtane.Migrations
+{
+    [DbContext(typeof(TenantDBContext))]
+    [Migration("Tenant.02.02.00.01")]
+    public class ChangeFolderNameAndPathColumnsSize : MultiDatabaseMigration
+    {
+        public ChangeFolderNameAndPathColumnsSize(IOqtaneDatabase database) : base(database)
+        {
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var folderEntityBuilder = new FolderEntityBuilder(migrationBuilder, ActiveDatabase);
+
+            folderEntityBuilder.AlterStringColumn("Name", 250);
+
+            // Drop the index is needed because the Path is already associated with IX_Folder
+            folderEntityBuilder.DropIndex("IX_Folder");
+            folderEntityBuilder.AlterStringColumn("Path", 1000);
+            folderEntityBuilder.AddIndex("IX_Folder", new[] { "SiteId", "Path" }, true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var folderEntityBuilder = new FolderEntityBuilder(migrationBuilder, ActiveDatabase);
+
+            folderEntityBuilder.AlterStringColumn("Name", 50);
+
+            folderEntityBuilder.DropIndex("IX_Folder");
+            folderEntityBuilder.AlterStringColumn("Path", 50);
+            folderEntityBuilder.AddIndex("IX_Folder", new[] { "SiteId", "Path" }, true);
+        }
+    }
+}
