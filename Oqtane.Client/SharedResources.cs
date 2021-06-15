f2b3d6bc5f695d19df68c159ed1e3bfe5ce8cd8a
Leigh <Leigh.Pointer@outlook.com>
2021-06-15 07:09:22 +0200

    SharedResources to reside at the project root.
    Namespace was changed from Oqtane to Oqtane.Client

---
+++ Oqtane.Client/SharedResources.cs
@@ -0,0 +1,13 @@
+namespace Oqtane.Client
+{
+    /// <summary>
+    /// Dummy class used to collect shared resource strings for this application
+    /// </summary>
+    /// <remarks>
+    /// This class is mostly used with IStringLocalizer and IHtmlLocalizer interfaces.
+    /// The class must reside at the project root.
+    /// </remarks>
+    public class SharedResources
+    {
+    }
+}
