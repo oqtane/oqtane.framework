57db7c1efc50a379a0e1ef5dbe96dd117c7e986e
Shaun Walker <shaun.walker@siliqon.com>
2021-09-22 11:55:01 -0400

    update version to 2.3.0 in preparation for release

--- Oqtane.Package/install.ps1
+++ Oqtane.Package/install.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path "..\Oqtane.Server\bin\Release\net5.0\publish\*" -DestinationPath "Oqtane.Framework.2.2.0.Install.zip" -Force 
+Compress-Archive -Path "..\Oqtane.Server\bin\Release\net5.0\publish\*" -DestinationPath "Oqtane.Framework.2.3.0.Install.zip" -Force 
