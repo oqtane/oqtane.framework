4cc0060c67c65b4b92a12d4e4b578b5311018f4b
Shaun Walker <shaun.walker@siliqon.com>
2023-01-14 14:45:30 -0500

    prepare for 3.3.1 patch

--- Oqtane.Package/upgrade.ps1
+++ Oqtane.Package/upgrade.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path "..\Oqtane.Server\bin\Release\net6.0\publish\*" -DestinationPath "Oqtane.Framework.3.3.0.Upgrade.zip" -Force 
+Compress-Archive -Path "..\Oqtane.Server\bin\Release\net6.0\publish\*" -DestinationPath "Oqtane.Framework.3.3.1.Upgrade.zip" -Force 
