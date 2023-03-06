13a2a77fd11787a778f79b795697eeb9f6670263
Leigh Pointer <leigh.pointer@outlook.com>
2023-03-06 18:41:15 +0100

    Revert "prepare for 3.4.0 release"
    This reverts commit d33f82d9690119795819570f849b1a1edc2480d5.

--- Oqtane.Package/install.ps1
+++ Oqtane.Package/install.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path "..\Oqtane.Server\bin\Release\net6.0\publish\*" -DestinationPath "Oqtane.Framework.3.4.0.Install.zip" -Force 
+Compress-Archive -Path "..\Oqtane.Server\bin\Release\net6.0\publish\*" -DestinationPath "Oqtane.Framework.3.3.1.Install.zip" -Force 
