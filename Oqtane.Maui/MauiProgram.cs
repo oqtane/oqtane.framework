7f7dff701986195663ca13bfaff64914b6f31668
sbwalker <shaun.walker@siliqon.com>
2023-05-15 13:21:30 -0400

    added AboutAssets.txt to Maui project and changed assembly references to Oqtane 4.0.0

--- Oqtane.Maui/MauiProgram.cs
+++ Oqtane.Maui/MauiProgram.cs
@@ -12,8 +12,8 @@ namespace Oqtane.Maui;
 public static class MauiProgram
 {
     // the API service url
-    static string apiurl = "https://www.dnfprojects.com"; // for testing
-    //static string apiurl = "http://localhost:44357"; // for local development (Oqtane.Server must be already running for MAUI client to connect)
+    //static string apiurl = "https://www.dnfprojects.com"; // for testing
+    static string apiurl = "http://localhost:44357"; // for local development (Oqtane.Server must be already running for MAUI client to connect)
 
     public static MauiApp CreateMauiApp()
 	{
