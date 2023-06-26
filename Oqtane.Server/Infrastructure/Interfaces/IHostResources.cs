6dd62a164e23e50f15726d9f9d55222d80fe33c9
sbwalker <shaun.walker@siliqon.com>
2023-06-26 08:11:46 -0400

    added deprecation message for IHostResources

--- Oqtane.Server/Infrastructure/Interfaces/IHostResources.cs
+++ Oqtane.Server/Infrastructure/Interfaces/IHostResources.cs
@@ -1,11 +1,12 @@
 using Oqtane.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Oqtane.Infrastructure
 {
-    // this interface is for declaring global resources and is useful for scenarios where you want to declare resources in a single location for the entire application
     public interface IHostResources
     {
+        [Obsolete("IHostResources is deprecated. Use module or theme scoped Resources in conjunction with ResourceLevel.Site instead.", false)]
         List<Resource> Resources { get; } // identifies global resources for an application
     }
 }
