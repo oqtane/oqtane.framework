65782e87c1eff5837bbf1d58636b8402f026dfb0
sbwalker <shaun.walker@siliqon.com>
2023-09-21 16:53:52 -0400

    add support for named site options

--- Oqtane.Server/Extensions/OqtaneSiteOptionsBuilder.cs
+++ Oqtane.Server/Extensions/OqtaneSiteOptionsBuilder.cs
@@ -17,10 +17,22 @@ namespace Microsoft.Extensions.DependencyInjection
         }
 
         public OqtaneSiteOptionsBuilder AddSiteOptions<TOptions>(
-            Action<TOptions, Alias, Dictionary<string, string>> action) where TOptions : class, new()
+            Action<TOptions, Alias, Dictionary<string, string>> configureOptions) where TOptions : class, new()
         {
             Services.TryAddSingleton<IOptionsMonitorCache<TOptions>, SiteOptionsCache<TOptions>>();
-            Services.AddSingleton<ISiteOptions<TOptions>, SiteOptions<TOptions>> (sp => new SiteOptions<TOptions>(action));
+            Services.AddSingleton<ISiteOptions<TOptions>, SiteOptions<TOptions>> (_ => new SiteOptions<TOptions>(configureOptions));
+            Services.TryAddTransient<IOptionsFactory<TOptions>, SiteOptionsFactory<TOptions>>();
+            Services.TryAddScoped<IOptionsSnapshot<TOptions>>(sp => BuildOptionsManager<TOptions>(sp));
+            Services.TryAddSingleton<IOptions<TOptions>>(sp => BuildOptionsManager<TOptions>(sp));
+
+            return this;
+        }
+
+        public OqtaneSiteOptionsBuilder AddSiteNamedOptions<TOptions>(string name,
+            Action<TOptions, Alias, Dictionary<string, string>> configureOptions) where TOptions : class, new()
+        {
+            Services.TryAddSingleton<IOptionsMonitorCache<TOptions>, SiteOptionsCache<TOptions>>();
+            Services.AddSingleton<ISiteNamedOptions<TOptions>, SiteNamedOptions<TOptions>>(_ => new SiteNamedOptions<TOptions>(name, configureOptions));
             Services.TryAddTransient<IOptionsFactory<TOptions>, SiteOptionsFactory<TOptions>>();
             Services.TryAddScoped<IOptionsSnapshot<TOptions>>(sp => BuildOptionsManager<TOptions>(sp));
             Services.TryAddSingleton<IOptions<TOptions>>(sp => BuildOptionsManager<TOptions>(sp));
