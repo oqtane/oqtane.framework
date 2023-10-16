68a47f9fc2e9ac738bf51ed3c2cd6afa917a1129
Cody <cody@decacon.com>
2023-10-16 11:59:30 -0700

    Fix formatting

--- Oqtane.Shared/Shared/PaneNames.cs
+++ Oqtane.Shared/Shared/PaneNames.cs
@@ -1,6 +1,6 @@
-namespace Oqtane.Shared 
+namespace Oqtane.Shared
 {
-    public class PaneNames 
+    public class PaneNames
     {
         public const string Admin = "Admin";
         public const string Default = "Default";
