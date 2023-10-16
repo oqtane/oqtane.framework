52dce29e950f8c1f49634581c4ea235f0f9ac10e
Cody <cody@decacon.com>
2023-10-16 12:03:07 -0700

    Remove unnecessary using directive and fixed formatting.

--- Oqtane.Shared/Shared/TenantNames.cs
+++ Oqtane.Shared/Shared/TenantNames.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Oqtane.Shared {
-    public class TenantNames {
+namespace Oqtane.Shared
+{
+    public class TenantNames
+    {
         public const string Master = "Master";
     }
 }
