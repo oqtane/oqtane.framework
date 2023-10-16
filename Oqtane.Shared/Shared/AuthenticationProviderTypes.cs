53dbcca263ab1045ca0c936dbed4674604689a38
Cody <cody@decacon.com>
2023-10-16 11:59:01 -0700

    Fix formatting

--- Oqtane.Shared/Shared/AuthenticationProviderTypes.cs
+++ Oqtane.Shared/Shared/AuthenticationProviderTypes.cs
@@ -1,6 +1,6 @@
-namespace Oqtane.Shared 
+namespace Oqtane.Shared
 {
-    public class AuthenticationProviderTypes 
+    public class AuthenticationProviderTypes
     {
         public const string OpenIDConnect = "oidc";
         public const string OAuth2 = "oauth2";
