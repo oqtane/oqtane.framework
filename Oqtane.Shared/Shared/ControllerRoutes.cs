b8cbcb795b85dd79f6ecd39b4c887a13d84c2e38
Cody <cody@decacon.com>
2023-10-16 11:53:43 -0700

    Fix formatting

--- Oqtane.Shared/Shared/ControllerRoutes.cs
+++ Oqtane.Shared/Shared/ControllerRoutes.cs
@@ -1,4 +1,5 @@
-﻿namespace Oqtane.Shared {
+﻿namespace Oqtane.Shared 
+{
     public class ControllerRoutes {
         public const string Default = "{alias}/api/[controller]";
         public const string ApiRoute = "api/[controller]";
