156d39490ec8d87e3e37fee25807ec443dca3fd0
sbwalker <shaun.walker@siliqon.com>
2024-02-01 09:47:46 -0500

    use new contants in .NET MAUI

--- Oqtane.Shared/Shared/RenderModes.cs
+++ Oqtane.Shared/Shared/RenderModes.cs
@@ -3,5 +3,6 @@ namespace Oqtane.Shared {
         public const string Static = "Static";
         public const string Interactive = "Interactive";
         public const string Headless = "Headless";
+        public const string Hybrid = "Hybrid"; // used at runtime by .NET MAUI clients
     }
 }
