e7157a852860e602b8ce69c8889042f7ad7e74a9
sbwalker <shaun.walker@siliqon.com>
2024-02-27 15:12:48 -0500

    replace Task.Run() logic in SiteService with async SiteRepository methods

--- Oqtane.Server/Repository/Interfaces/ISiteRepository.cs
+++ Oqtane.Server/Repository/Interfaces/ISiteRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Oqtane.Models;
 
 namespace Oqtane.Repository
@@ -6,11 +7,17 @@ namespace Oqtane.Repository
     public interface ISiteRepository
     {
         IEnumerable<Site> GetSites();
+        Task<IEnumerable<Site>> GetSitesAsync();
         Site AddSite(Site site);
+        Task<Site> AddSiteAsync(Site site);
         Site UpdateSite(Site site);
+        Task<Site> UpdateSiteAsync(Site site);
         Site GetSite(int siteId);
+        Task<Site> GetSiteAsync(int siteId);
         Site GetSite(int siteId, bool tracking);
+        Task<Site> GetSiteAsync(int siteId, bool tracking);
         void DeleteSite(int siteId);
+        Task DeleteSiteAsync(int siteId);
         void InitializeSite(Alias alias);
         void CreatePages(Site site, List<PageTemplate> pageTemplates, Alias alias);
     }
