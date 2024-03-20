7b67c9aa8db8289bc0ba6e9022b41f49464ace53
Ben <zyhfish@163.com>
2024-03-20 15:46:32 +0800

    Fix #4025: add user setting to keep the view mode.

---
+++ Oqtane.Shared/Models/UserSetting.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oqtane.Models
+{
+    public class UserSetting
+    {
+        public const string ViewMode = "Page:ViewMode";
+
+        public const string LastViewPage = "LastViewed";
+    }
+}
