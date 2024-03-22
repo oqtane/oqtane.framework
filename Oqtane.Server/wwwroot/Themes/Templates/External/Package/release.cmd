a8ffc414feb35a36be3fdcf3d1df35df2c2b2ac4
sbwalker <shaun.walker@siliqon.com>
2024-03-22 15:31:55 -0400

    fix theme template release.,cmd to copy package to /Packages folder

--- Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.cmd
+++ Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.cmd
@@ -1,3 +1,3 @@
 del "*.nupkg"
 "..\..\[RootFolder]\oqtane.package\nuget.exe" pack [Owner].Theme.[Theme].nuspec 
-XCOPY "*.nupkg" "..\..\[RootFolder]\Oqtane.Server\wwwroot\Themes\" /Y
+XCOPY "*.nupkg" "..\..\[RootFolder]\Oqtane.Server\wwwroot\Packages\" /Y
