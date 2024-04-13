c58647c685fbea1e02aa8d5b67d66879d55ee5fe
Leigh Pointer <leigh-pointer@users.noreply.github.com>
2024-04-13 13:23:46 +0200

    Upgraded to Bootstrap 5.3.3
    Update Oqtane Theme to 5.3.3
    Reworked Blazor theme to load Resources from Info class.
    Bootstrap 5.3.3 caused issue with Close button [x] appearing next to the Title and not in the top right corner.
    Updated Theme Template

--- Oqtane.Client/Themes/BlazorTheme/ThemeInfo.cs
+++ Oqtane.Client/Themes/BlazorTheme/ThemeInfo.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Oqtane.Documentation;
 using Oqtane.Models;
+using Oqtane.Shared;
 
 namespace Oqtane.Themes.BlazorTheme
 {
@@ -9,7 +11,14 @@ namespace Oqtane.Themes.BlazorTheme
         public Theme Theme => new Theme
         {
             Name = "Blazor Theme",
-            Version = "1.0.0"
+            Version = "1.0.0",
+            Resources = new List<Resource>()
+            {
+		        // obtained from https://cdnjs.com/libraries
+                new Resource { ResourceType = ResourceType.Stylesheet, Url = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css", Integrity = "sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==", CrossOrigin = "anonymous" },
+                new Resource { ResourceType = ResourceType.Stylesheet, Url = "~/Theme.css" },
+                new Resource { ResourceType = ResourceType.Script, Url = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js", Integrity = "sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==", CrossOrigin = "anonymous", Location = ResourceLocation.Body }
+            }
         };
 
     }
