73ea17ae0f655f47d49f0f81a47ba936c4aabb34
sbwalker <shaun.walker@siliqon.com>
2024-10-21 14:51:20 -0400

    changes to migrate Oqtane to .NET 9 and version 6.0.0

--- Oqtane.Server/Extensions/DbContextOptionsBuilderExtensions.cs
+++ Oqtane.Server/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Oqtane.Databases.Interfaces;
 // ReSharper disable ConvertToUsingDeclaration
 
@@ -9,7 +10,8 @@ namespace Oqtane.Extensions
     {
         public static DbContextOptionsBuilder UseOqtaneDatabase([NotNull] this DbContextOptionsBuilder optionsBuilder, IDatabase database, string connectionString)
         {
-            database.UseDatabase(optionsBuilder, connectionString);
+            database.UseDatabase(optionsBuilder, connectionString)
+                .ConfigureWarnings(warnings => warnings.Log(RelationalEventId.PendingModelChangesWarning));
 
             return optionsBuilder;
         }
