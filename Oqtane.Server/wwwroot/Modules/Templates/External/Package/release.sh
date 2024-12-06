5ace34b5cd0f307842fdacdb70b7d4de7ef0c314
Leigh Pointer <leigh-pointer@users.noreply.github.com>
2024-12-06 13:37:37 +0100

    Add the $TargetFramework macro to the Release Builds
    Add the $TargetFramework macro to the Release Builds

--- Oqtane.Server/wwwroot/Modules/Templates/External/Package/release.sh
+++ Oqtane.Server/wwwroot/Modules/Templates/External/Package/release.sh
@@ -1,2 +1,4 @@
-"..\..\oqtane.framework\oqtane.package\nuget.exe" pack [Owner].Module.[Module].nuspec 
+TargetFramework=$1
+
+"..\..\oqtane.framework\oqtane.package\nuget.exe" pack [Owner].Module.[Module].nuspec -Properties targetframework=$TargetFramework
 cp -f "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\Packages\"
--- Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.sh
+++ Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.sh
@@ -1,2 +1,4 @@
-"..\..\oqtane.framework\oqtane.package\nuget.exe" pack [Owner].Theme.[Theme].nuspec 
+TargetFramework=$1
+
+"..\..\oqtane.framework\oqtane.package\nuget.exe" pack [Owner].Theme.[Theme].nuspec -Properties targetframework=$TargetFramework
 cp -f "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\Packages\"
