1a4380dcd7cf6701243c79c3270e6db28f8d68fc
Leigh Pointer <leigh-pointer@users.noreply.github.com>
2024-12-07 15:02:27 +0100

    Variable update

--- Oqtane.Server/wwwroot/Modules/Templates/External/Package/release.cmd
+++ Oqtane.Server/wwwroot/Modules/Templates/External/Package/release.cmd
@@ -2,6 +2,6 @@
 set TargetFramework=%1
 
 del "*.nupkg"
-"..\..\[RootFolder]\oqtane.package\nuget.exe" pack [Owner].Module.[Module].nuspec -Properties targetframework=$(TargetFramework)
+"..\..\[RootFolder]\oqtane.package\nuget.exe" pack [Owner].Module.[Module].nuspec -Properties targetframework=%TargetFramework%
 XCOPY "*.nupkg" "..\..\[RootFolder]\Oqtane.Server\Packages\" /Y
 
--- Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.cmd
+++ Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.cmd
@@ -2,5 +2,5 @@
 set TargetFramework=%1
 
 del "*.nupkg"
-"..\..\[RootFolder]\oqtane.package\nuget.exe" pack [Owner].Theme.[Theme].nuspec -Properties targetframework=$(TargetFramework)
+"..\..\[RootFolder]\oqtane.package\nuget.exe" pack [Owner].Theme.[Theme].nuspec -Properties targetframework=%TargetFramework%
 XCOPY "*.nupkg" "..\..\[RootFolder]\Oqtane.Server\wwwroot\Packages\" /Y
