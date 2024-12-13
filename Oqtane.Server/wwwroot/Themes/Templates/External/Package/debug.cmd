07bac0792530096b093389c866753b7c3310d8f7
Leigh Pointer <leigh-pointer@users.noreply.github.com>
2024-12-13 11:58:13 +0100

    Updated to use the ProjectName in the Templates

--- Oqtane.Server/wwwroot/Themes/Templates/External/Package/debug.cmd
+++ Oqtane.Server/wwwroot/Themes/Templates/External/Package/debug.cmd
@@ -1,6 +1,7 @@
 @echo off
 set TargetFramework=%1
+set ProjectName=%2
 
-XCOPY "..\Client\bin\Debug\%TargetFramework%\[Owner].Theme.[Theme].Client.Oqtane.dll" "..\..\[RootFolder]\Oqtane.Server\bin\Debug\%TargetFramework%\" /Y
-XCOPY "..\Client\bin\Debug\%TargetFramework%\[Owner].Theme.[Theme].Client.Oqtane.pdb" "..\..\[RootFolder]\Oqtane.Server\bin\Debug\%TargetFramework%\" /Y
+XCOPY "..\Client\bin\Debug\%TargetFramework%\%ProjectName%.Client.Oqtane.dll" "..\..\[RootFolder]\Oqtane.Server\bin\Debug\%TargetFramework%\" /Y
+XCOPY "..\Client\bin\Debug\%TargetFramework%\%ProjectName%.Client.Oqtane.pdb" "..\..\[RootFolder]\Oqtane.Server\bin\Debug\%TargetFramework%\" /Y
 XCOPY "..\Client\wwwroot\*" "..\..\[RootFolder]\Oqtane.Server\wwwroot\" /Y /S /I
