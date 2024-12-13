141378f3364525acc5d6f4426d8452ddf3e86ce1
Leigh Pointer <leigh-pointer@users.noreply.github.com>
2024-12-13 11:21:13 +0100

    Update release.sh

--- Oqtane.Server/wwwroot/Modules/Templates/External/Package/release.sh
+++ Oqtane.Server/wwwroot/Modules/Templates/External/Package/release.sh
@@ -1,4 +1,4 @@
 TargetFramework=$1
 
-"..\..\[RootFolder]\oqtane.package\nuget.exe" pack %ProjectName%.nuspec-Properties targetframework=%TargetFramework%;projectname=%ProjectName%
+"..\..\[RootFolder]\oqtane.package\nuget.exe" pack %ProjectName%.nuspec -Properties targetframework=%TargetFramework%;projectname=%ProjectName%
 cp -f "*.nupkg" "..\..\[RootFolder]\Oqtane.Server\Packages\"
