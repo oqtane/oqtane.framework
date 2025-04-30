753ab3bdd75f7c7db263e271be8bdccae7083878
sbwalker <shaun.walker@siliqon.com>
2025-04-30 13:46:52 -0400

    resolve issue with host setting overrides

--- Oqtane.Server/Repository/Interfaces/ISettingRepository.cs
+++ Oqtane.Server/Repository/Interfaces/ISettingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Oqtane.Models;
+using Oqtane.Shared;
 
 namespace Oqtane.Repository
 {
@@ -7,6 +8,7 @@ namespace Oqtane.Repository
     {
         IEnumerable<Setting> GetSettings(string entityName);
         IEnumerable<Setting> GetSettings(string entityName, int entityId);
+        IEnumerable<Setting> GetSettings(string entityName1, int entityId1, string entityName2, int entityId2);
         Setting AddSetting(Setting setting);
         Setting UpdateSetting(Setting setting);
         Setting GetSetting(string entityName, int settingId);
