52799c7cb05cc0ac8a94506a063db6104669806e
sbwalker <shaun.walker@siliqon.com>
2025-08-22 08:41:52 -0400

    declare dependencies in nuspec files and optimize application template

--- Oqtane.Package/release.cmd
+++ Oqtane.Package/release.cmd
@@ -1,12 +1,12 @@
 del "*.nupkg"
 del "*.zip"
+rmdir /Q/S "..\Oqtane.Server\bin\Release\net9.0\publish"
 dotnet clean -c Release ..\Oqtane.sln 
 dotnet build -c Release ..\Oqtane.sln
 nuget.exe pack Oqtane.Client.nuspec
 nuget.exe pack Oqtane.Server.nuspec
 nuget.exe pack Oqtane.Shared.nuspec                  
 nuget.exe pack Oqtane.Framework.nuspec
-rmdir /Q/S "..\Oqtane.Server\bin\Release\net9.0\publish"
 dotnet publish ..\Oqtane.Server\Oqtane.Server.csproj /p:Configuration=Release
 rmdir /Q/S "..\Oqtane.Server\bin\Release\net9.0\publish\Content"
 rmdir /Q/S "..\Oqtane.Server\bin\Release\net9.0\publish\wwwroot\Content"
