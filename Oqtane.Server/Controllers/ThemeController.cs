95ec163f2c691243853c5b25ca4ba5997dbda3a7
sbwalker <shaun.walker@siliqon.com>
2025-10-10 15:23:04 -0400

    upgrade to .NET 10 RC1

--- Oqtane.Server/Controllers/ThemeController.cs
+++ Oqtane.Server/Controllers/ThemeController.cs
@@ -304,8 +304,8 @@ namespace Oqtane.Controllers
                     return new Dictionary<string, object>()
                             {
                                 { "FrameworkVersion", Constants.Version },
-                                { "ClientReference", $"<Reference Include=\"Oqtane.Client\"><HintPath>..\\..\\{rootFolder}\\Oqtane.Server\\bin\\Debug\\net9.0\\Oqtane.Client.dll</HintPath></Reference>" },
-                                { "SharedReference", $"<Reference Include=\"Oqtane.Shared\"><HintPath>..\\..\\{rootFolder}\\Oqtane.Server\\bin\\Debug\\net9.0\\Oqtane.Shared.dll</HintPath></Reference>" },
+                                { "ClientReference", $"<Reference Include=\"Oqtane.Client\"><HintPath>..\\..\\{rootFolder}\\Oqtane.Server\\bin\\Debug\\net10.0\\Oqtane.Client.dll</HintPath></Reference>" },
+                                { "SharedReference", $"<Reference Include=\"Oqtane.Shared\"><HintPath>..\\..\\{rootFolder}\\Oqtane.Server\\bin\\Debug\\net10.0\\Oqtane.Shared.dll</HintPath></Reference>" },
                             };
                 });
             }
--- Oqtane.Server/Program.cs
+++ Oqtane.Server/Program.cs
@@ -1,25 +1,46 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
-using Oqtane.Infrastructure;
 using Microsoft.Extensions.Logging;
-using Oqtane.Documentation;
+using Oqtane.Extensions;
+using Oqtane.Infrastructure;
+using Oqtane.Shared;
 
 namespace Oqtane.Server
 {
-    [PrivateApi("Mark Entry-Program as private, since it's not very useful in the public docs")]
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
-            var databaseManager = host.Services.GetService<IDatabaseManager>();
+            var builder = WebApplication.CreateBuilder(args);
+
+            AppDomain.CurrentDomain.SetData(Constants.DataDirectory, Path.Combine(builder.Environment.ContentRootPath, "Data"));
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+            var configuration = configurationBuilder.Build();
+
+            builder.Services.AddOqtane(configuration, builder.Environment);
+
+            var app = builder.Build();
+
+            var corsService = app.Services.GetRequiredService<ICorsService>();
+            var corsPolicyProvider = app.Services.GetRequiredService<ICorsPolicyProvider>();
+            var syncManager = app.Services.GetRequiredService<ISyncManager>();
+
+            app.UseOqtane(configuration, builder.Environment, corsService, corsPolicyProvider, syncManager);
+
+            var databaseManager = app.Services.GetService<IDatabaseManager>();
             var install = databaseManager.Install();
             if (!string.IsNullOrEmpty(install.Message))
             {
-                var filelogger = host.Services.GetRequiredService<ILogger<Program>>();
+                var filelogger = app.Services.GetRequiredService<ILogger<Program>>();
                 if (filelogger != null)
                 {
                     filelogger.LogError($"[Oqtane.Server.Program.Main] {install.Message}");
@@ -27,18 +48,8 @@ namespace Oqtane.Server
             }
             else
             {
-                host.Run();
+                app.Run();
             }
         }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseConfiguration(new ConfigurationBuilder()
-                    .AddCommandLine(args)
-                    .AddEnvironmentVariables()
-                    .Build())
-                .UseStartup<Startup>()
-                .ConfigureLocalizationSettings()
-                .Build();
     }
 }
