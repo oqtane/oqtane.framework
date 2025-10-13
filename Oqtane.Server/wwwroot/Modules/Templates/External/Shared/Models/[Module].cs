02c4da553901fe0e4f6e4aaea710071fa11e9dcf
sbwalker <shaun.walker@siliqon.com>
2025-10-13 11:50:46 -0400

    use ModelBase in module template

--- Oqtane.Server/wwwroot/Modules/Templates/External/Shared/Models/[Module].cs
+++ Oqtane.Server/wwwroot/Modules/Templates/External/Shared/Models/[Module].cs
@@ -1,4 +1,3 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Oqtane.Models;
@@ -6,16 +5,11 @@ using Oqtane.Models;
 namespace [Owner].Module.[Module].Models
 {
     [Table("[Owner][Module]")]
-    public class [Module] : IAuditable
+    public class [Module] : ModelBase
     {
         [Key]
         public int [Module]Id { get; set; }
         public int ModuleId { get; set; }
         public string Name { get; set; }
-
-        public string CreatedBy { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public string ModifiedBy { get; set; }
-        public DateTime ModifiedOn { get; set; }
     }
 }
