7ef43763636723bd7c40a125333da4093f832f2c
sbwalker <shaun.walker@siliqon.com>
2025-10-15 10:37:04 -0400

    add internal theme template to application template

---
+++ Oqtane.Application/Server/wwwroot/Themes/Templates/Internal/Client/Themes/[Owner].Theme.[Theme]/ThemeInfo.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Oqtane.Models;
+using Oqtane.Themes;
+using Oqtane.Shared;
+
+namespace [Owner].Theme.[Theme]
+{
+    public class ThemeInfo : ITheme
+    {
+        public Oqtane.Models.Theme Theme => new Oqtane.Models.Theme
+        {
+            Name = "[Owner] [Theme]",
+            Version = "1.0.0",
+            PackageName = "[Owner].Theme.[Theme]",
+            ThemeSettingsType = "[Owner].Theme.[Theme].ThemeSettings, [Owner].Theme.[Theme].Client.Oqtane",
+            ContainerSettingsType = "[Owner].Theme.[Theme].ContainerSettings, [Owner].Theme.[Theme].Client.Oqtane",
+            Resources = new List<Resource>()
+            {
+		// obtained from https://cdnjs.com/libraries
+                new Stylesheet(Constants.BootstrapStylesheetUrl, Constants.BootstrapStylesheetIntegrity, "anonymous"),
+                new Stylesheet("~/Theme.css"),
+                new Script(Constants.BootstrapScriptUrl, Constants.BootstrapScriptIntegrity, "anonymous")
+            }
+        };
+
+    }
+}
