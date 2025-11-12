57159c89c36d4d972e897314cf8abb7c2b08de8c
David Montesinos <mdmontesinos@hotmail.com>
2025-11-12 12:11:19 +0100

    Use new BlazorWebView interception to redirect assets to Oqtane server

--- Oqtane.Maui/MainPage.xaml.cs
+++ Oqtane.Maui/MainPage.xaml.cs
@@ -6,4 +6,31 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private static readonly byte[] _htmlPlaceholder = System.Text.Encoding.UTF8.GetBytes("<html><body>Asset Redirect</body></html>");
+
+    private void BlazorWebView_WebResourceRequested(object sender, WebViewWebResourceRequestedEventArgs e)
+    {
+        if (e.Uri.Host != "0.0.0.1") return;
+
+        var path = e.Uri.AbsolutePath;
+
+        if (!path.Contains('.') || path.Contains("_framework")) return;
+
+        e.Handled = true;
+
+        //TODO: Get api url from configuration as well
+        var redirectUrl = $"{MauiConstants.ApiUrl.TrimEnd('/')}{e.Uri.PathAndQuery}";
+
+        // Create a minimal HTML body (some engines require non-empty content)
+        using var stream = new MemoryStream(_htmlPlaceholder);
+
+        // Set redirect headers
+        var headers = new Dictionary<string, string>(e.Headers)
+        {
+            ["Location"] = redirectUrl
+        };
+
+        e.SetResponse(code: 302, reason: "Found", headers: headers, content: stream);
+    }
 }
