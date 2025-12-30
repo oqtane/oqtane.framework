2844b793fa8a9323d26b63d5ead232a0b3b3f0c8
sbwalker <shaun.walker@siliqon.com>
2025-12-30 13:43:11 -0500

    increment version to 10.0.4

--- Oqtane.Package/install.ps1
+++ Oqtane.Package/install.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path "..\Oqtane.Server\bin\Release\net10.0\publish\*" -DestinationPath "Oqtane.Framework.10.0.3.Install.zip" -Force 
+Compress-Archive -Path "..\Oqtane.Server\bin\Release\net10.0\publish\*" -DestinationPath "Oqtane.Framework.10.0.4.Install.zip" -Force 
