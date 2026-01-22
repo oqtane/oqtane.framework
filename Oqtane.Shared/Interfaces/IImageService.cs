6548af34cb25bc52162c86211d1dc1c7f2442c80
David Montesinos <mdmontesinos@hotmail.com>
2026-01-22 17:27:48 +0100

    Image optimization to webp without manipulation

--- Oqtane.Shared/Interfaces/IImageService.cs
+++ Oqtane.Shared/Interfaces/IImageService.cs
@@ -2,8 +2,11 @@ namespace Oqtane.Services
 {
     public interface IImageService
     {
+        public const int DefaultQuality = 60;
+
         public string[] GetAvailableFormats();
 
         public string CreateImage(string filepath, int width, int height, string mode, string position, string background, string rotate, string format, string imagepath);
+        public string OptimizeImageToWebp(string filepath, int quality, string imagepath);
     }
 }
