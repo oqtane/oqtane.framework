0d4d51448eda5f42475a6cfca6fc12708794553d
sbwalker <shaun.walker@siliqon.com>
2026-02-20 15:11:54 -0500

    allow LanguageSwitcher to support culture and ui culture

--- Oqtane.Server/Migrations/Tenant/10010002_AddCultureCode.cs
+++ Oqtane.Server/Migrations/Tenant/10010002_AddCultureCode.cs
@@ -18,9 +18,11 @@ namespace Oqtane.Migrations.Tenant
         {
             var siteEntityBuilder = new SiteEntityBuilder(migrationBuilder, ActiveDatabase);
             siteEntityBuilder.AddStringColumn("CultureCode", 10, true);
+            siteEntityBuilder.UpdateData("CultureCode", $"'{Shared.Constants.DefaultCulture}'");
 
             var userEntityBuilder = new UserEntityBuilder(migrationBuilder, ActiveDatabase);
             userEntityBuilder.AddStringColumn("CultureCode", 10, true);
+            userEntityBuilder.UpdateData("CultureCode", $"'{Shared.Constants.DefaultCulture}'");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
