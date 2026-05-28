6cba355198b7f758db6d7b9fb832e7576c9a4745
sbwalker <shaun.walker@siliqon.com>
2026-05-28 13:19:51 -0400

    disable Swagger on new installations by default

--- Oqtane.Server/Controllers/SystemController.cs
+++ Oqtane.Server/Controllers/SystemController.cs
@@ -53,7 +53,7 @@ namespace Oqtane.Controllers
                     systeminfo.Add("DetailedErrors", _configManager.GetSetting("DetailedErrors", "false"));
                     systeminfo.Add("Logging:LogLevel:Default", _configManager.GetSetting("Logging:LogLevel:Default", "Information"));
                     systeminfo.Add("Logging:LogLevel:Notify", _configManager.GetSetting("Logging:LogLevel:Notify", "Error"));
-                    systeminfo.Add("UseSwagger", _configManager.GetSetting("UseSwagger", "true"));
+                    systeminfo.Add("UseSwagger", _configManager.GetSetting("UseSwagger", "false"));
                     systeminfo.Add("Caching:Duration", _configManager.GetSetting("Caching:Duration", "10"));
                     systeminfo.Add("Caching:FailSafe", _configManager.GetSetting("Caching:FailSafe", "True"));
                     systeminfo.Add("Caching:Distributed", _configManager.GetSetting("Caching:Distributed", "False"));
