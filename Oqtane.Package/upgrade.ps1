58300b7c8f0ae2ca532209c341fd17dcc39ef5b6
sbwalker <shaun.walker@siliqon.com>
2026-06-09 15:39:02 -0400

    increment version to 10.2.1

--- Oqtane.Package/upgrade.ps1
+++ Oqtane.Package/upgrade.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path "..\Oqtane.Server\bin\Release\net10.0\publish\*" -DestinationPath "Oqtane.Framework.10.2.0.Upgrade.zip" -Force 
+Compress-Archive -Path "..\Oqtane.Server\bin\Release\net10.0\publish\*" -DestinationPath "Oqtane.Framework.10.2.1.Upgrade.zip" -Force 
