0ec3c2d5a61fcc488e5c209b9bd78a9b86f79576
Mark Davis <github@nohorse.com>
2026-07-15 19:27:23 -0700

    localize page names in menu and breadcrumb theme controls
    Page names are localized using the page name as the static resource
    key. The current theme's resources are searched first so that theme
    developers can provide their own page name translations, then shared
    resources (following the same convention as the Admin Dashboard),
    falling back to the raw page name when no resource exists.

--- Oqtane.Client/Themes/Controls/Theme/MenuItemsBase.cs
+++ Oqtane.Client/Themes/Controls/Theme/MenuItemsBase.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 using Oqtane.Models;
 using Oqtane.UI;
@@ -10,11 +11,51 @@ namespace Oqtane.Themes.Controls
 {
     public abstract class MenuItemsBase : MenuBase
     {
+        [Inject]
+        protected IStringLocalizerFactory LocalizerFactory { get; set; }
+
+        [Inject]
+        protected IStringLocalizer<SharedResources> SharedLocalizer { get; set; }
+
         [Parameter()]
         public Page ParentPage { get; set; }
 
         [Parameter()]
         public IEnumerable<Page> Pages { get; set; }
+
+        private IStringLocalizer _themeLocalizer;
+        private string _themeType;
+
+        protected override void OnParametersSet()
+        {
+            if (_themeType != PageState.Page.ThemeType)
+            {
+                _themeType = PageState.Page.ThemeType;
+                try
+                {
+                    _themeLocalizer = LocalizerFactory.Create(_themeType);
+                }
+                catch
+                {
+                    _themeLocalizer = null; // theme type could not be resolved
+                }
+            }
+        }
+
+        protected virtual string GetName(Page page)
+        {
+            // page names are localized using the page name as the static resource key - the current theme's resources are searched first
+            // so that theme developers can provide their own translations, then shared resources (following the same convention as the Admin Dashboard)
+            if (_themeLocalizer != null)
+            {
+                var name = _themeLocalizer[page.Name];
+                if (!name.ResourceNotFound)
+                {
+                    return name;
+                }
+            }
+            return SharedLocalizer[page.Name];
+        }
 
         protected IEnumerable<Page> GetChildPages()
         {
